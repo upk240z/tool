1d225031ea379c1b73c20307e4676798cf20a947
Y.Hasegawa <hasegawa@zaf.jp>
2024-01-04 02:30:03 +0900

    base64

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,6 +4,7 @@ export const MENU_LINKS = [
   {name: 'JSON parser', href: '/json', class: 'text-base'},
   {name: 'YAML parser', href: '/yaml', class: 'text-base'},
   {name: 'URL encode/decode', href: '/url', class: 'text-base'},
+  {name: 'Base64', href: '/base64', class: 'text-base'},
   {name: 'Crypt', href: '/crypt', class: 'text-base'},
   {name: 'QRCode', href: '/qrcode', class: 'text-base'},
   {name: 'Fixed length data', href: '/fld', class: 'text-base'},
---
+++ src/routes/base64/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import {Base64} from 'js-base64'
+  import { Textarea } from 'flowbite-svelte'
+  import ResultBox from '$lib/components/ResultBox.svelte'
+
+  let encodeTarget = ''
+  let encoded = ''
+  let decodeTarget = ''
+  let decoded = ''
+
+  $: if (encodeTarget.length > 0) {
+    encoded = Base64.encode(encodeTarget)
+  } else {
+    encoded = ''
+  }
+
+  $: if (decodeTarget.length > 0) {
+    decoded = Base64.decode(decodeTarget)
+  } else {
+    decoded = ''
+  }
+</script>
+
+<h1>Base64</h1>
+
+<section>
+  <Textarea rows="4" placeHolder="Encode target" bind:value={encodeTarget}/>
+  <ResultBox label="Encoded" text={encoded}/>
+</section>
+
+<hr class="my-5">
+
+<section>
+  <Textarea rows="4" placeHolder="Decode target" bind:value={decodeTarget}/>
+  <ResultBox label="Decoded" text={decoded}/>
+</section>
